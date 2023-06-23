041ae9ca84699428b61fda2127c91cbb5cad8819
nickmullaney <nick.mullaney@gmail.com>
2023-06-23 11:18:15 -0700

    class fixes

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,43 +1,44 @@
-import React from 'react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
 import App from '../App';
 
+// from the docs: https://testing-library.com/docs/react-testing-library/example-intro/#full-example
 const server = setupServer(
-  rest.post('/test', (req, res, ctx) => {
-    return res(
-      ctx.json({ result: 'fake thing 1' }),
-      ctx.status(200)
-    );
+  rest.get('/testGet', (req, res, ctx) => {
+    return res(ctx.json({greeting: 'hello there'}))
   }),
-);
+)
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// establish API mocking BEFORE running the tests below
+beforeAll(() => server.listen())
+
+// reset any handlers that are declared as part of our tests
+// i.e. for testing one-time error scenarios
+afterEach(() => server.resetHandlers())
+
+// clean up after tests are done.  turn. things. off.
+afterAll(() => server.close())
 
 describe('App component integration', () => {
   it('allows form use and renders expected results', async () => {
     render(<App />);
-
     let urlInput = screen.getByTestId('form-input');
-    let postTest = screen.getByTestId('post-test');
+    let getSpan = screen.getByTestId('get-test');
     let button = screen.getByTestId('form-button');
 
-    fireEvent.change(urlInput, { target: { value: 'test.com' } });
-    fireEvent.click(postTest);
+    fireEvent.change(urlInput, {target: {value: '/testGet'}});
+    fireEvent.click(getSpan);
     fireEvent.click(button);
 
-    await screen.findByTestId('results-pre');
-
-    let pre = screen.getByTestId('results-pre');
-    expect(pre).toHaveTextContent('fake thing 1');
+    let pre = await screen.findByTestId('results-pre');
+    expect(pre).toHaveTextContent('hello there');
 
     let methodDiv = screen.getByTestId('app-div-method');
     let urlDiv = screen.getByTestId('app-div-url');
-    expect(methodDiv).toHaveTextContent('POST');
-    expect(urlDiv).toHaveTextContent('test.com');
+    expect(methodDiv).toHaveTextContent('GET');
+    expect(urlDiv).toHaveTextContent('/testGet')
+
   });
 });
